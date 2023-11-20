74d8179f27bcb4c2bc1d39b8ff628665acbabd22
Iamneo <techadmin@iamneo.ai>
2023-11-20 13:00:50 +0000

    Backup Commit

--- dotnetapp/Program.cs
+++ dotnetapp/Program.cs
@@ -9,7 +9,18 @@ var connectionString=builder.Configuration.GetConnectionString("mycon");
 builder.Services.AddDbContext<ApplicationDbContext>(options=>options.UseSqlServer(connectionString));
 
 // Add services to the container.
-
+builder.Services.AddCors(
+   
+    options=>{
+        options.AddDefaultPolicy(
+       
+        builder=>{
+            builder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
+       
+ 
+    });
+   
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
