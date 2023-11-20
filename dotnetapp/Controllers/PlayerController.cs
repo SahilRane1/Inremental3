cfb9e91d9f5d7317f93e62c5a8ef719767d92b1a
Iamneo <techadmin@iamneo.ai>
2023-11-20 06:15:51 +0000

    Backup Commit

---
+++ dotnetapp/Controllers/PlayerController.cs
@@ -0,0 +1,22 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using dotnetapp.Models;
+
+namespace dotnetapp.Controllers
+{
+    
+
+    public class PlayerController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PlayerController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        
+    }
+}
